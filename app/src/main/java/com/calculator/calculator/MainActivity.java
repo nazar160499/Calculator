65f7924a955ddc63df3f240d24870c7e3d8597c9
nazar <nazar160499@gmail.com>
2016-12-17 17:24:19 +0200

    clean code

--- app/src/main/java/com/calculator/calculator/MainActivity.java
+++ app/src/main/java/com/calculator/calculator/MainActivity.java
@@ -8,10 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     EditText mFirstNumEditText;
     EditText mSecondNumEditText;
 
@@ -23,33 +20,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mFirstNumEditText = (EditText) findViewById(R.id.edit_text1);
         mSecondNumEditText = (EditText) findViewById(R.id.edit_text2);
 
-        Button button_sum = (Button) findViewById(R.id.button1);
-        button_sum.setOnClickListener(this);
+        Button buttonSum = (Button) findViewById(R.id.button1);
+        buttonSum.setOnClickListener(this);
 
-        Button button_difference = (Button) findViewById(R.id.button2);
-        button_difference.setOnClickListener(this);
+        Button buttonDifference = (Button) findViewById(R.id.button2);
+        buttonDifference.setOnClickListener(this);
 
-        Button button_dobytok = (Button) findViewById(R.id.button3);
-        button_dobytok.setOnClickListener(this);
+        Button buttonDobytok = (Button) findViewById(R.id.button3);
+        buttonDobytok.setOnClickListener(this);
 
-        Button button_devision = (Button) findViewById(R.id.button4);
-        button_devision.setOnClickListener(this);
+        Button buttonDevision = (Button) findViewById(R.id.button4);
+        buttonDevision.setOnClickListener(this);
 
         mResultTextView = (TextView) findViewById(R.id.text_view);
     }
 
     @Override
     public void onClick(View view) {
-
             float firstNum1 = getFirstNum();
             float secondNum2 = getSecondNum();
             float result = 0;
-
-
 
         switch(view.getId()) {
             case R.id.button1:
@@ -68,35 +61,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     oper = "/";
                     result = firstNum1/secondNum2;
                     break;
-                default:
+            default:
                     break;
             }
             mResultTextView.setText( firstNum1 + " " + oper + " " + secondNum2 + " = " + result);
+    }
 
-        }
     private float getFirstNum() {
         String number = mFirstNumEditText.getText().toString().trim();
-        if (number.isEmpty())
-        {
+        if (number.isEmpty()) {
             return 0;
         }
-        else
-        {
+        else {
             return Float.parseFloat(number);
         }
     }
 
-        private float getSecondNum() {
-            String number = mSecondNumEditText.getText().toString().trim();
-            if(number.isEmpty())
-            {
-                return 0;
-            }
-            else
-            {
-                return Float.parseFloat(number);
-            }
-
+    private float getSecondNum() {
+        String number = mSecondNumEditText.getText().toString().trim();
+        if(number.isEmpty()) {
+            return 0;
+        }
+        else {
+            return Float.parseFloat(number);
         }
     }
+}
 
